146e4a6295a0f72e44dc5830f4bc5e270f687d47
sairam852 <racsai574@gmail.com>
2021-10-28 15:41:56 +0530

    setup completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # oneNeuron
 oneNeuron implementation
+
+# References-
+[python official docs](https://packaging.python.org/tutorials/packaging-projects/)
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME="oneNeuron"
+USER_NAME="sairam852"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email=racsai574@gmail.com",
+    description="it is an implimentation of perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{oneNeuron}",
+    project_urls={
+        "Bug Tracker": "https://github.com/{USER_NAME}/{oneNeuron}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
